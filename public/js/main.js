1db46bf607d848e2312a5de8d63754c644fad9eb
Vien Van <vienvonvan@gmail.com>
2016-03-13 13:32:05 -0700

    added cancel button

--- public/js/main.js
+++ public/js/main.js
@@ -3,4 +3,10 @@ console.log("This is connected");
 
 $('#get-started').click(function() {
   $('.signup').toggleClass('hidden');
+  $('#home-btns').addClass('hidden');
+})
+
+$('.cancel').click(function() {
+  $('.signup').toggleClass('hidden');
+  $('#home-btns').removeClass('hidden');
 })
