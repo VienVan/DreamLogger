5a8bf4b5ea6ec6b76bfc3cd1a2671538c2f93a5b
Vien Van <vienvonvan@gmail.com>
2016-03-14 11:50:33 -0700

    added uploadcare widget to signup form and attached url to dreamer.img

--- controllers/dreamersController.js
+++ controllers/dreamersController.js
@@ -5,9 +5,16 @@ var dreamersController = {
 		res.render('dreamers/index');
 	},
 	create: function(req, res){
+		var dreamer = req.body.dreamer;
+		var username = dreamer.username;
+		var password = dreamer.password;
+		console.log(req.body);
+		console.log(dreamer);
+		var img = dreamer.img;
+		console.log(img);
 		Dreamer.create({username, password}, function(err, dreamer) {
-			var id = dreamer._id;
-			console.log(id);
+			// var id = dreamer._id;
+			// console.log(id);
 			err ? console.log(err) : res.redirect('/dreamers/'+id+'/dreams')
 		});
 	},
