7bf379019c128485b866c2f657096c7aad53b74a
Vien Van <vienvonvan@gmail.com>
2016-03-15 18:19:57 -0700

    Created tags.dreams methods

--- controllers/dreamsController.js
+++ controllers/dreamsController.js
@@ -1,44 +1,33 @@
-var Dream = require('../models').Dream;
-var Dreamer = require('../models').Dreamer;
-var Tag = require('../models/tag');
-var DreamTag = require('../models').DreamTag;
+var Dream 							= require('../models').Dream;
+var Dreamer 						= require('../models').Dreamer;
+var Tag 								= require('../models/tag');
+var DreamTag 						= require('../models').DreamTag;
 
-var dreamsController = {
+var dreamsController 		= {
 	index: function(req, res) {
 		var id 							= req.params.id;
-		console.log(req.params.id);
 		Dreamer.findById({_id: id}, function(err, dreamer) {
 			Dream.find({dreamerId: id}, function(err, dreams) {
-
-				res.render('dreams/index', {dreamer: dreamer, dreams: dreams})
+					res.render('dreams/index', {dreamer: dreamer, dreams: dreams})
+				})
 			})
-			// res.render('dreams/index', {dreamer: dreamer});
-		});
-
 	},
 	create: function(req, res) {
+		var description 		= req.body.description;
+		var dreamerId     	= req.body.dreamerId;
+		var newdream     	 	= {description: description, dreamerId: dreamerId};
+		var newTag        	= req.body.tags;
+		var tagId;
 
-				var description = req.body.description;
-				// console.log(req.body);
-				// var tag = req.body.tag;
-				var description = req.body.description;
-				var dreamerId 	= req.body.dreamerId;
-				var newdream 		= {description: description, dreamerId: dreamerId};
-				var newTag 			= {name: req.body.tags.name};
-				var tagQuery 		=
-				// console.log(req.body.tags);
+			console.log('req.body.tags',newTag);
 
-					Dream.create(newdream, function(err, newdream) {
-						console.log(newdream);
-
-							Tag.create(newTag, function(err, tag) {
+				Dream.create(newdream, function(err, newdream) {
+						Tag.create(newTag, function(err, tag) {
+							DreamTag.create({dreamId: newdream._id, tagId: tag._id}, function(err, dreamtag) {
 								res.json(newdream);
 							});
-
-				});
-
-
-
+						});
+		});
 	},
 	update: function(req, res) {
 
